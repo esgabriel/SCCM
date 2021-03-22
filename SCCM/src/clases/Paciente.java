c975327833db8d3c56055c4685fc976a8aa9f670
Gabriel Reyes Cruz <57185758+zS18015542@users.noreply.github.com>
2021-03-21 20:31:14 -0600

    Correción clase Paciente

--- SCCM/src/clases/Paciente.java
+++ SCCM/src/clases/Paciente.java
@@ -1,8 +1,19 @@
 package clases;
 
-public class Paciente {
+public class Paciente extends Persona{
 
     private int numeroSeguroSocial;
+    
+    public Paciente(int numeroSeguroSocial) {
+        super();
+        this.numeroSeguroSocial = numeroSeguroSocial;
+    }
+    
+    public Paciente(int numeroSeguroSocial, String nombre, String aP, String aM, String fechaNacimiento)
+    {
+        super(nombre, aP, aM, fechaNacimiento);
+        this.numeroSeguroSocial = numeroSeguroSocial;
+    }
 
     public void agregar() {
 
@@ -14,8 +25,12 @@ public class Paciente {
 
     @Override
     public String toString() {
-        return "Paciente{" + "numeroSeguroSocial=" + numeroSeguroSocial + '}';
+        String paciente = super.toString() + "," + this.numeroSeguroSocial ;
+
+        return paciente;
     }
+
+
 
     public void modificar() {
 
