c8c73570e1d796d3ee622a476d6fb93ab8bca54c
Gabriel Reyes Cruz <57185758+zS18015542@users.noreply.github.com>
2021-03-21 20:06:27 -0600

    Clase Persona

--- SCCM/src/clases/Persona.java
+++ SCCM/src/clases/Persona.java
@@ -1,14 +1,63 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package clases;
 
-/**
- *
- * @author TheHu
- */
 public class Persona {
-    
+
+    private String nombre;
+    private String aP;
+    private String aM;
+    private String fechaNacimiento;
+
+    public Persona() {
+        this.nombre = "";
+        this.aP = "";
+        this.aM = "";
+        this.fechaNacimiento = "";
+    }
+
+    public Persona(String nombre, String aP, String aM, String fechaNacimiento) {
+        this.nombre = nombre;
+        this.aP = aP;
+        this.aM = aM;
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getaP() {
+        return aP;
+    }
+
+    public void setaP(String aP) {
+        this.aP = aP;
+    }
+
+    public String getaM() {
+        return aM;
+    }
+
+    public void setaM(String aM) {
+        this.aM = aM;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    @Override
+    public String toString() {
+        String persona = nombre + "," + aP + "," + aM + "," + fechaNacimiento;
+
+        return persona;
+    }
+
 }
