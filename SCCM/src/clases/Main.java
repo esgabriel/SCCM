6c46335385c13e05298a9561d41d3136f32cb1ea
Jose Angel Rincon Martinez <57199695+S18015524@users.noreply.github.com>
2021-03-29 23:51:31 -0600

    Creado la clase principal

---
+++ SCCM/src/clases/Main.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+import guisistema.Menu;
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author PC
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        crearDocumentos();
+        Menu mp = new Menu();
+        mp.setVisible(true);
+    }
+    
+    private static void crearDocumentos() {
+        String documentos = System.getenv("USERPROFILE");
+        
+        File carpeta = new File(documentos + "\\Documents\\DB");
+
+        if (carpeta.mkdir()) {
+            ArrayList<String> horario = new ArrayList();
+
+            for (int i = 0; i < 24; i++) {
+                String hora = i + ",false,false,false,false,false,false,false";
+                horario.add(hora);
+            }
+            DB baseDato = new DB();
+            baseDato.guardarTxt(horario, "HorarioDB.txt");
+        }
+    }
+}
